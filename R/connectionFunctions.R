896de93563df766038ea40510a0b2faeecb1c480
donenrique <donenrique@32f441e3-671c-4264-bccf-b733008c0ec4>
2020-05-22 15:47:17 +0000

    First commit

---
+++ R/connectionFunctions.R
@@ -0,0 +1,89 @@
+########################################################
+# Basic R function for the package.
+# Author: Mathieu Fortin, Canadian Wood Fibre Centre
+# Date: April 2019
+########################################################
+
+.welcomeMessage <- function() {
+  packageStartupMessage("Welcome to betadiv package! This package implements estimators of beta diversity indices.")
+  packageStartupMessage("For more information, visit https://sourceforge.net/p/mrnfforesttools/divindices/wiki/Home/ .")
+}
+
+
+.onAttach <- function(libname, pkgname) {
+  .welcomeMessage()
+}
+
+.onUnload <- function(libpath) {
+  shutdownJava()
+}
+
+.onDetach <- function(libpath) {
+  shutdownJava()
+}
+
+
+#'
+#' Extends the shutdownJava function of the J4R package
+#'
+#' @export
+shutdownJava <- function() {
+  if (J4R::isConnectedToJava()) {
+    J4R::shutdownJava()
+  }
+}
+
+.addToArray <- function(refArray, array) {
+  if (length(refArray) != length(array)) {
+    stop("Incompatible array length!")
+  } else {
+    for (i in 1:length(array)) {
+      refArray[[i]] <- c(refArray[[i]], array[[i]])
+    }
+  }
+  return(refArray)
+}
+
+.convertJavaDataSetIntoDataFrame <- function(dataSetObject) {
+  refArray <- NULL
+  observations <- J4R::callJavaMethod(dataSetObject, "getObservations")
+  observations <- J4R::getAllValuesFromListObject(observations)
+  for (obs in observations) {
+    array <- J4R::callJavaMethod(obs, "toArray")
+    array <- as.list(J4R::getAllValuesFromArray(array))
+    if (is.null(refArray)) {
+      refArray <- array
+    } else {
+      refArray <- .addToArray(refArray, array)
+    }
+  }
+  dataFrame <- NULL
+  for (i in 1:length(refArray)) {
+    dataFrame <- as.data.frame(cbind(dataFrame, refArray[[i]]))
+  }
+  colnames(dataFrame) <- J4R::getAllValuesFromListObject(J4R::callJavaMethod(dataSetObject, "getFieldNames"))
+  return(dataFrame)
+}
+
+
+.loadREpicea <- function() {
+  if (!J4R::checkIfClasspathContains("repicea.jar")) {
+    J4R::addUrlToClassPath("repicea.jar", packageName = "betadiv")
+  }
+}
+
+.connectToBetadivLibrary <- function() {
+  if (!J4R::isConnectedToJava()) {
+    J4R::connectToJava()
+  }
+  .loadREpicea()
+  .loadBetadiv()
+}
+
+
+.loadBetadiv <- function() {
+  if (!J4R::checkIfClasspathContains("betadiversityindices.jar")) {
+    J4R::addUrlToClassPath("betadiversityindices.jar", packageName = "betadiv")
+  }
+}
+
